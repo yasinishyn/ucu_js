92936ea6ff683d327dce4e23e6f87c8204bfba79
Andrew Yasinishyn <ay@Andrews-MacBook-Air.local>
2021-03-17 10:37:14 +0200

    add_js

---
+++ 1_lecture/add_js/README.md
@@ -0,0 +1,11 @@
+## Run with Simplest Node.js server
+
+```
+$ npm install http-server -g
+```
+
+in add_js root folder run :
+
+```
+$ http-server
+```
---
+++ 1_lecture/quiz_1_snippets.js
@@ -0,0 +1,13 @@
+
+const pets = {
+    markus: {
+        species: 'Fat Cat',
+        age: 4,
+    },
+    pluton: {
+        species: 'Loud Cat',
+        age: 5,
+    }
+}
+console.log(pets.markus.age)
+
